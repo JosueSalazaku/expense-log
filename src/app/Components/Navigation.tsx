9eb8884d69ffff56f5a9ada2e588519e1cfc434a
Josué Salazaku <josuesalazaku@gmail.com>
2024-09-14 15:42:00 +0200

    add SignedIn and SignedOut auth

--- src/app/Components/Navigation.tsx
+++ src/app/Components/Navigation.tsx
@@ -1,16 +1,39 @@
-import Link from 'next/link'
-import React from 'react'
+"use client";
+
+import Link from 'next/link';
+import React from 'react';
+import { SignedIn, SignedOut, UserButton, useAuth } from '@clerk/nextjs';
+import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation';
 
 export default function Navigation() {
+  const { signOut } = useAuth();
+  const router = useRouter();  
+
+  const handleSignOut = async () => {
+    await signOut();
+    router.push('/'); 
+  };
+
   return (
     <nav className='w-screen h-[120px] flex flex-row justify-between items-center bg-zinc-900'>
       <Link href="/" className='text-2xl font-bold'>Logo</Link>
       <ul className='flex flex-row justify-center items-center font-bold gap-10 text-xl w-full'>
-        <li><Link href="/dashboard">Overview</Link></li>
-        <li><Link href="/expenses">Expenses</Link></li>
-        <li><Link href="/sign-in">Sign In</Link></li>
-        <li><Link href="/sign-up">Sign Up</Link></li>
+        <SignedIn> 
+          <li><Link href="/overview">Overview</Link></li>
+          <li><Link href="/expenses">Expenses</Link></li>
+          <li><UserButton /></li>
+          <li>
+            <Button onClick={handleSignOut} className='font-bold text-xl bg-zinc-500'>
+              Sign Out
+            </Button>
+          </li>
+        </SignedIn>
+        <SignedOut>
+          <li><Link href="/sign-in">Sign In</Link></li>
+          <li><Link href="/sign-up">Sign Up</Link></li>
+        </SignedOut>
       </ul>
     </nav>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs"
 
 export default function Home() {
   return (
     <main className="w-screen h-screen flex flex-col justify-center items-center">
-    <h1>EXPENSE TRACKER/LOG</h1>
+    <SignedIn>
+        <h1>EXPENSE TRACKER/LOG</h1>
+    </SignedIn>
+    <SignedOut>
+        <h1>
+          WELCOM I GUESSE
+        </h1>
+    </SignedOut>
     </main>
   );
 }
