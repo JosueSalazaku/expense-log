6f138ec4dc8df743f7f4c15406646e2441f4140c
Josué Salazaku <josuesalazaku@gmail.com>
2024-09-14 07:14:12 +0200

    Update global CSS and add base and create sign-in/up pages

---
+++ src/app/Components/Navigation.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Navigation() {
+  return (
+    <nav className='w-screen h-[120px] flex flex-row justify-center item-center bg-zinc-900'>
+      Navigation
+    </nav>
+  )
+}
+
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
+import Navigation from "./Components/Navigation";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,7 +30,8 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        >
+       <Navigation />  
         {children}
       </body>
       </html>
